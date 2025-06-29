516861b597394a37d562c599a742ecc12686cc33
Gokul PM <mail.gokulpm@gmail.com>
2025-06-29 14:22:32 +0530

    feat: etcd int

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"encoding/json"
+	"strconv"
 	"time"
 )
 
@@ -23,4 +24,24 @@ func (d *Duration) UnmarshalJSON(b []byte) error {
 
 func (d *Duration) Value() time.Duration {
 	return time.Duration(*d)
+}
+
+type EtcdInt int
+
+func (i *EtcdInt) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	value, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+	*i = EtcdInt(value)
+	return nil
+}
+
+func (i *EtcdInt) Value() int {
+	return int(*i)
 }
