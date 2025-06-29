6f23375d2aa7e33ff1eab9d8034b4a49f3a7c365
Gokul PM <mail.gokulpm@gmail.com>
2025-06-29 14:53:45 +0530

    feat: string size

--- types.go
+++ types.go
@@ -3,6 +3,7 @@ package types
 import (
 	"encoding/json"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -22,8 +23,8 @@ func (s *StringDuration) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-func (d *StringDuration) Value() time.Duration {
-	return time.Duration(*d)
+func (s *StringDuration) Value() time.Duration {
+	return time.Duration(*s)
 }
 
 type StringInt int
@@ -44,4 +45,45 @@ func (s *StringInt) UnmarshalJSON(b []byte) error {
 
 func (s *StringInt) Value() int {
 	return int(*s)
+}
+
+type StringSize float64
+
+func (s *StringSize) UnmarshalJSON(b []byte) error {
+	var v string
+	err := json.Unmarshal(b, &v)
+	if err != nil {
+		return err
+	}
+	parsed, err := parseSize(v)
+	if err != nil {
+		return err
+	}
+	*s = StringSize(parsed)
+	return nil
+}
+
+func (s *StringSize) Value() float64 {
+	return float64(*s)
+}
+
+var sizeMap = map[string]int64{
+	"B": 1,
+	"K": 1 << 10,
+	"M": 1 << 20,
+	"G": 1 << 30,
+}
+
+func parseSize(v string) (float64, error) {
+	for unit, size := range sizeMap {
+		if strings.HasSuffix(v, unit) {
+			n := strings.TrimSuffix(v, unit)
+			f, err := strconv.ParseFloat(n, 64)
+			if err != nil {
+				return 0, err
+			}
+			return f * float64(size), nil
+		}
+	}
+	return 0, nil
 }
