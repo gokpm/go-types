3f78281fe8c58af88f5bc7b98c93a612c158cbc3
Gokul PM <mail.gokulpm@gmail.com>
2025-06-29 11:29:34 +0530

    feat: duration

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gokpm/go-types
+
+go 1.24.4
---
+++ types.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Duration time.Duration
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	parsed, err := time.ParseDuration(s)
+	if err != nil {
+		return err
+	}
+	*d = Duration(parsed)
+	return nil
+}
